61d98d774cc58e13e7df5fdef352bd19eff81f02
unknown <hikaruvlad@gmail.com>
2024-05-19 22:48:47 +0300

    finished the game

--- minesweep.py
+++ minesweep.py
@@ -4,6 +4,7 @@ import random
 number_of_mines = 5
 field = None
 player_field = None
+revealed_tiles = 0
 field_x = 5
 field_y = 5
 
@@ -63,6 +64,34 @@ def create_field():
                 field[i][j] = weight
 
 
+def reveal(i: int, j: int):
+    global revealed_tiles
+    
+    if player_field[i][j] == "?":
+        if field[i][j] == "m":
+            return False
+        player_field[i][j] = field[i][j]
+        revealed_tiles += 1
+        if field[i][j] == 0:
+            if i > 0 and j > 0: 
+                reveal(i-1, j-1)
+            if i > 0: 
+                reveal(i-1, j)
+            if i > 0 and j < (field_x - 1): 
+                reveal(i-1, j+1)
+            if j > 0: 
+                reveal(i, j-1)
+            if j < (field_x - 1): 
+                reveal(i, j+1)
+            if i < (field_y - 1) and j > 0: 
+                reveal(i+1, j-1)
+            if i < (field_y - 1): 
+                reveal(i+1, j)
+            if i < (field_y - 1) and j < (field_x - 1): 
+                reveal(i+1, j+1)
+        return True
+    
+
 def print_field():
     for i in range(0, field_x):
         for j in range(0, field_y):
@@ -77,9 +106,28 @@ def print_classified_field():
             print(field[i][j], end="")
             
         print()
+        
+        
+def game_loop():
+    create_field()
+    print_field()
 
+    while True:
+        print("Enter i: ", end="")
+        i = input()
+        print("Enter j: ", end="")
+        j = input()
+        res = reveal(int(i - 1), int(j - 1))
+        
+        if not res:
+            print("BOOM!")
+            break
+        
+        if revealed_tiles == field_x * field_y - number_of_mines:
+            print("WIN!")
+            break
+        
+        print_field()
+        
 
-create_field()
-print_classified_field()
-print("-------")
-print_field()
+game_loop()
